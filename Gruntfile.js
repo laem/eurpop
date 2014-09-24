ab0bf4b316fb93761243a2f3c4703dc0cc234780
Mael Thomas <mael@qunb.com>
2014-09-24 13:34:52 +0200

    favicon changed

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,12 +67,6 @@ module.exports = function (grunt) {
       }
     },
 
-    karma: {
-      unit: {
-        configFile: 'karma.conf.js'
-      }
-    },
-
     copy: {
       dist: {
         files: [
@@ -119,8 +113,6 @@ module.exports = function (grunt) {
       'webpack-dev-server'
     ]);
   });
-
-  grunt.registerTask('test', ['karma']);
 
   grunt.registerTask('build', ['clean', 'copy', 'webpack']);
 
